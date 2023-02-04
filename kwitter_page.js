3e9a51e40d1bc2b350cf5e45833a1b2760283227
MasViv <112710248+MasViv@users.noreply.github.com>
2023-02-04 09:59:42 -0500

    Add files via upload

---
+++ kwitter_page.js
@@ -0,0 +1,36 @@
+const firebaseConfig = {
+      apiKey: "AIzaSyASbR9YfDOAfmSvUVlu3Zadv2qaVG-u4m0",
+      authDomain: "stickchat-1ef6c.firebaseapp.com",
+      databaseURL: "https://stickchat-1ef6c-default-rtdb.firebaseio.com",
+      projectId: "stickchat-1ef6c",
+      storageBucket: "stickchat-1ef6c.appspot.com",
+      messagingSenderId: "516132291796",
+      appId: "1:516132291796:web:8b2f46a902b01775896741"
+    };
+
+    const app = initializeApp(firebaseConfig);
+
+    function logout()
+    {
+      window.location="index.html";
+      localStorage.removeItem("user_name");
+      localStorage.removeItem("room_name");
+    }
+
+function getData() { firebase.database().ref("/"+room_name).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
+         firebase_message_id = childKey;
+         message_data = childData;
+
+function send()
+{
+      msg=document.getElementById("msg").value;
+      firebase.database().ref(room_name).push({
+            name:user_name,
+            message:msg,
+            like:0
+      });
+      document.getElementById("msg").value="";
+}
+
+      } });  }); }
+getData();
